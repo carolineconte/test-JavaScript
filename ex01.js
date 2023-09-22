29b213f280255cc7246c9cd698191362293b5775
Caroline Conte <132572031+carolineconte@users.noreply.github.com>
2023-09-22 13:14:19 +0200

    Create ex01.js

---
+++ ex01.js
@@ -0,0 +1,42 @@
+function podeSerConvertidoParaNumero(str) {
+    const numero = parseFloat(str);
+    return !isNaN(numero);
+}
+
+
+function analizzaSpese(str) {
+
+    //let importoTotale = 0;
+    let obj = {}
+    let nomereparto = ''
+    let importo = 0
+
+    // Cicliamo attraverso le righe
+    for (let value of str) {
+        nomereparto += value
+
+        if (value == ' ') {
+
+            if (!podeSerConvertidoParaNumero(nomereparto)) {
+                obj[nomereparto] = importo
+                 importo = 0
+            }
+            if (podeSerConvertidoParaNumero(nomereparto)) {
+                importo += parseFloat(nomereparto)
+                console.log(importo)
+            }
+
+            nomereparto = ''
+           
+        }
+    }
+
+    return obj
+}
+
+// Esempio di utilizzo:
+const stringaSpese = 'Reparto1 100 200 Reparto2 200 Reparto3 50 Reparto4 300';
+
+
+const risultati = analizzaSpese(stringaSpese);
+console.log(risultati);
